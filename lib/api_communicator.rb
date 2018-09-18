2b1b95c024175eafa75d823bad1f7b40c2cdcd70
nachoespino53 <nachoespino53@gmail.com>
2018-09-18 15:35:47 -0400

    Done

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,29 +6,39 @@ def get_character_movies_from_api(character)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
-  # This is done for educational purposes. This is not typically done in code.
 
+  char_info = get_character_info(response_hash, character)
+  char_films = char_info["films"]
 
-  # iterate over the response hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  films_hash = char_films.map do |url|
+    get_film_info(url)
+  end
 end
 
 def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+  titles = films_hash.map do |films|
+    films["title"]
+  end
+  titles.each_with_index do |title, index|
+    puts "#{index + 1}. #{title}"
+  end
 end
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
   print_movies(films_array)
+end
+
+def get_character_info(char_hash, character)
+  new_hash = char_hash["results"]
+  new_hash.find do |char|
+    char["name"].downcase == character.downcase
+  end
+end
+
+def get_film_info(url)
+  response_string = RestClient.get(url)
+  response_hash = JSON.parse(response_string)
 end
 
 ## BONUS
--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,8 +1,14 @@
 def welcome
   # puts out a welcome message here!
+  puts "Welcome to Star Wars character search"
 end
+
+
 
 def get_character_from_user
   puts "please enter a character"
   # use gets to capture the user's input. This method should return that input, downcased.
+  search_character = gets.chomp
+  puts "\n\n"
+  search_character
 end
